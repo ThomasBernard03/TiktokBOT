71c930272d97f1c389a3c41a2af704ca84209816
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-06-04 17:36:27 +0200

    create methods to dowload and split video

--- src/main.py
+++ src/main.py
@@ -7,20 +7,43 @@ from moviepy.editor import VideoFileClip
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
+def download_video(url):
+    video = YouTube(url)
+    video_stream = video.streams.get_highest_resolution()
+
+
+    with tqdm(total=video_stream.filesize) as pbar:
+        video_stream.download(filename='video.mp4')
+        pbar.update(video_stream.filesize)
+
+
+def split_video(duration):
+    num_parts = int(video_duration / duration)
+
+
+    for i in range(num_parts):
+        start_time = i * duration
+        end_time = (i + 1) * duration
+
+        part = video.subclip(start_time, end_time)
+        part.write_videofile(f"part{i+1}.mp4")
+
+
+    if video_duration % duration != 0:
+        start_time = num_parts * duration
+        end_time = video_duration
+
+        last_part = video.subclip(start_time, end_time)
+        last_part.write_videofile(f"part{num_parts + 1}.mp4")
+
+
+
 url = input("Enter your youtube video URL : ")
+download_video(url)
 
 
-video = YouTube(url)
-video_stream = video.streams.get_highest_resolution()
 
-
-with tqdm(total=video_stream.filesize) as pbar:
-    video_stream.download(filename='video.mp4')
-    pbar.update(video_stream.filesize)
-
-
-video_path = "video.mp4"  
-video = VideoFileClip(video_path)
+video = VideoFileClip("video.mp4" )
 video_duration = video.duration
 
 
@@ -29,21 +52,12 @@ if value < 10 or value > video_duration:
     print("The value entered is invalid. Please enter a duration between 10s and the video duration.")
     exit()
 
-
-num_parts = int(video_duration / value)
-
-
-for i in range(num_parts):
-    start_time = i * value
-    end_time = (i + 1) * value
-
-    part = video.subclip(start_time, end_time)
-    part.write_videofile(f"part{i+1}.mp4")
+split_video(value)
 
 
-if video_duration % value != 0:
-    start_time = num_parts * value
-    end_time = video_duration
 
-    last_part = video.subclip(start_time, end_time)
-    last_part.write_videofile(f"part{num_parts + 1}.mp4")
+
+
+
+
+
