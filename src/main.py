8daa60072cf74d0191b7a9fa799ce9b5c6e6173c
Thomas Bernard <thomas.bernard.310@gmail.com>
2023-06-04 16:39:50 +0200

    can download a video

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
-from pytube import YouTube
+from pytube import YouTube 
+import ssl 
+from tqdm import tqdm # for progression bar
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 
 url = input("Enter your youtube video URL : ")
 
 
 video = YouTube(url)
-
-
 video_stream = video.streams.get_highest_resolution()
-video_stream.download()
+
+
+with tqdm(total=video_stream.filesize) as pbar:
+    video_stream.download(filename='video.mp4')
+    pbar.update(video_stream.filesize)
